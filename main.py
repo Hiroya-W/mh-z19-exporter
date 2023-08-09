1ae96e8a67fb551ba8c5b823596a8dae6a8db410
Hiroya-W <hyuyukun@gmail.com>
2023-08-09 15:24:58 +0900

    :sparkles: Add mh-z19 exporter

---
+++ main.py
@@ -0,0 +1,13 @@
+import time
+
+import mh_z19
+from prometheus_client import Gauge, start_http_server
+
+co2_metric = Gauge("co2", "CO2 concentration in ppm")
+
+if __name__ == "__main__":
+    start_http_server(9000)
+    while True:
+        res = mh_z19.read()
+        co2_metric.set(res["co2"])
+        time.sleep(1)
